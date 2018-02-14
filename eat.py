e29c477a94eac35e8de0b4e140d0362c5ca45b34
edmond120 <edmondwong888@msn.com>
2018-02-14 16:21:27 -0500

    done

--- eat.py
+++ eat.py
@@ -8,10 +8,19 @@ def get_rests_by_borough(b):
     return rests.find({"borough" : b})
 
 def get_rests_by_zipcode(z):
-	return rests.address.find({'zipcode' : str(z)})
+	return rests.find({'address.zipcode' : str(z)})
+
+def get_rests_by_zipcode_and_grade(z,g):
+	return rests.find({'address.zipcode' : str(z), 'grades.grade' : str(g)})
+
+def get_rests_by_zipcode_and_score_below_a_specific_threshold(z,t):
+	return rests.find({'address.zipcode' : str(z), 'grades.0.score' : {'$lt' : t}})
+
+def get_rests_with_score_above_a_specific_threshold_or_with_a_specific_score(t,s):
+	return rests.find({'$or' : [{'grades.0.score' : {'$gt' : t}},{'grades.0.score' : s}]})
 
 def main():
-    x = get_rests_by_zipcode(10460)
+    x = get_rests_with_score_above_a_specific_threshold_or_with_a_specific_score(5,2)
     for a in x:
         print a
 
