6639c7ba7db681d7ad33b7b26b70c4f6a8838283
Jake Zaia <jzaia@stuy.edu>
2018-02-22 12:55:55 -0500

    Add function to get pokemon info from API

--- poke.py
+++ poke.py
@@ -1,28 +1,32 @@
 # Team PokeMongo
 
 '''
-The data we are using is from pokeapi.co. It is a list of all the pokemon and their stats and abilities.
+The data we are using is from pokeapi.co. It is a list of all the pokemon and their stats and abilities. We will be storing this information for the 1st 151 pokemon in the database.
 '''
 
 import urllib2, json
-
 from pymongo import MongoClient
 
+## Some globals
 connection = MongoClient("homer.stuy.edu")
 db = connection.PokeMongo
+api_url = "http://pokeapi.co/api/v2/"
 
 
-def get_pokemon_data(pid):
-  url = urllib2.urlopen('https://pokeapi.co/api/v2/pokemon/' + str(pid))
-  return url.read()
-
+def get_poke_data(num):
+    url = api_url + 'pokemon/' + str(num)
+    opener = urllib2.build_opener()
+    opener.addheaders = [('User-agent', 'Mozilla/5.0')]
+    obj = opener.open(url)
+    obj = obj.read()
+    return json.loads(obj)
 
 def insert_pokemon():
   pass
 
 
 def main():
-  print get_pokemon_data(1)
+  print get_poke_data(1)
 
 
 if __name__ == '__main__':
